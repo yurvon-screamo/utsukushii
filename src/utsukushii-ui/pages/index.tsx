37fe44cceb8ca3e625aa941ac3d354ba52eab93a
yurvon-screamo <yurvon@ya.ru>
2024-08-09 11:02:46 +0300

    on next js build

--- src/utsukushii-ui/pages/index.tsx
+++ src/utsukushii-ui/pages/index.tsx
@@ -1,23 +1,11 @@
 import TestReport, { TestReportProps } from "@/components/component/test-report";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import { promises as fs } from 'fs';
-import { ReportContent } from "@/lib/model";
+import raw from "@/data.json"
 
-export const getServerSideProps: GetServerSideProps<TestReportProps> = async () => {
-  const file = await fs.readFile(process.cwd() + '/public/data.json', 'utf8');
-  const content: ReportContent = JSON.parse(file);
-
-  return {
-    props: {
-      content: content,
-    },
-  };
-};
-
-export default function Page(props: TestReportProps) {
+export default function Page() {
+  const jsonView = JSON.stringify(raw)
   return (  
     <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      <TestReport content={props.content} />
+      <TestReport content={JSON.parse(jsonView)} />
     </div>
   );
 }
