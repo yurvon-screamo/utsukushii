b4c5656931f01fb632fec38ebe77aead7b6407f3
yurvon-screamo <yurvon@ya.ru>
2024-08-11 19:28:59 +0300

    todo build

---
+++ utsukushii_ui/staticBuild/_next/static/lQkzO2PZyffOcg6xfJUfJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/231-504f71a924275e79.js","static/chunks/pages/index-6dbdcf7bd91b94df.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
